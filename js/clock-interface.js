cfc6753623136ae97292a9fa57cb7981821351fb
Erik Killops and Jonathan Thom <jonathan@>
2017-01-03 09:32:09 -0800

    Add alarm to clock

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -3,4 +3,11 @@ var Clock = require('./../js/clock.js').clockModule;
 $(document).ready(function(){
   var clock = new Clock();
   $('#time').text(clock.time);
+
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var alarm = $("#alarm").val();
+    clock.setAlarm(alarm);
+    $(".alarms").text(clock.alarm);
+  });
 });
--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,10 @@
 function Clock() {
   this.time = moment().format('LT');
+  this.alarm;
+}
+
+Clock.prototype.setAlarm = function(time) {
+  this.alarm = time;
 }
 
 exports.clockModule = Clock;
